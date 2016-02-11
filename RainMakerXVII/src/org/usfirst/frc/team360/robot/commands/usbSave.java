3df4ac5aed4ecabdac5176bb164f01233249082c
FRCTeam360 <programmersFRC360@gmail.com>
2016-02-10 17:51:02 -0800

    Original Log File

--- RainMakerXVII/src/org/usfirst/frc/team360/robot/commands/usbSave.java
+++ RainMakerXVII/src/org/usfirst/frc/team360/robot/commands/usbSave.java
@@ -1,70 +1,3 @@
-/*package org.usfirst.frc.team360.robot.commands;
-
-import edu.wpi.first.wpilibj.VictorSP;
-import edu.wpi.first.wpilibj.command.Command;
-import java.io.*;
-
-import org.usfirst.frc.team360.robot.Robot;
-import org.usfirst.frc.team360.robot.RobotMap;
-
-public class usbSave extends Command {
-	VictorSP motorR = RobotMap.motorR1;  
-	VictorSP motorL = RobotMap.motorR2;  
-	VictorSP motorRL = RobotMap.motorL1;
-	VictorSP motorLR = RobotMap.motorL2;  
-	FileOutputStream Textfile;
-	ObjectOutputStream save;
-	protected void initialize() {
-		
-		try{
-			 Textfile=new FileOutputStream("u/usbSaveFile.txt");
-			
-			// Might need to change saveFile to u/usbSaveFile
-			 save = new ObjectOutputStream(Textfile);
-
-			
-			
-			System.out.println("Test!");
-			           
-			}
-		
-			catch(Exception etc){
-			exc.printStackTrace();
-		}
-	}
-	  protected boolean isFinished() {
-	        return true;
-	    }
-	@Override
-	protected void execute() {
-		// TODO Auto-generated method stub
-		try {
-			save.writeObject(motorR.get());
-			save.writeObject(motorL.get());
-			save.writeObject(motorRL.get());
-			save.writeObject(motorLR.get());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	@Override
-	protected void end() {
-		// TODO Auto-generated method stub
-		try {
-			save.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	@Override
-	protected void interrupted() {
-		// TODO Auto-generated method stub
-		
-	}
-}*/
-
 package org.usfirst.frc.team360.robot.commands;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Command;
@@ -109,10 +42,6 @@ public class usbSave extends Command {
 		protected void execute() {
 			try {
 				byte[] contentInBytes = content.getBytes();
-				save.writeObject(motorR.get());
-				save.writeObject(motorL.get());
-				save.writeObject(motorRL.get());
-				save.writeObject(motorLR.get());
 				fop.write(contentInBytes);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
